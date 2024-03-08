8105e3c1a076b51569d21c727070a7395272e2ec
jovanplavsic <jovanplavsic05@gmail.com>
2024-03-07 22:18:54 -0600

    fully functioning ajax track upload system

--- static/js/workspace.js
+++ static/js/workspace.js
@@ -34,20 +34,22 @@ document.addEventListener('DOMContentLoaded', function () {
         if (cropper) {
             // If cropper is initialized, get the cropped image blob
             cropper.getCroppedCanvas().toBlob((blob) => {
-                sendFormData(blob);
+                sendProjectData(blob);
                 cropper.destroy();
                 cropper = null; // Reset cropper to null
             });
         } else {
             // If no image was chosen for cropping, just send the form data
-            sendFormData();
+            sendProjectData();
         }
     });
 
-    function sendFormData(blob = null) {
+    function sendProjectData(blob = null) {
         console.log('clicked', blob);
         const fd = new FormData(projectForm);
         fd.append('csrfmiddlewaretoken', csrf[0].value);
+        fd.append('form_type', 'project')
+
         if (blob) {
             fd.append('coverimg', blob, 'pfp.png');
         }
@@ -74,4 +76,56 @@ document.addEventListener('DOMContentLoaded', function () {
             processData: false,
         });
     }
+
+    const trackForm = document.getElementById('TrackForm')
+    const mp3Input = document.getElementById('id_mp3')
+
+    mp3Input.addEventListener('change', (e) => {
+        console.log('track chosen');
+
+        const modal = document.getElementById('codeModal')
+        let newTrack = document.createElement('div');
+
+        let trackId = 'temp';
+        newTrack.id = `vis-container-${trackId}`;
+
+        const lastChild = modal.children[modal.children.length - 1];
+        modal.insertBefore(newTrack, lastChild);
+
+        // modal.append(newTrack)
+        console.log(mp3Input.value)
+
+        // Create a Blob URL from the file
+        if (e.target.files && e.target.files[0]) {
+            const mp3Url = URL.createObjectURL(e.target.files[0]);
+            createVis('temp', mp3Url);
+        }
+
+        const fd = new FormData(trackForm);
+        fd.append('csrfmiddlewaretoken', csrf[0].value);
+        fd.append('mp3', mp3Input.value);
+        fd.append('form_type', 'track')
+
+        $.ajax({
+            type: 'POST',
+            url: trackForm.action,
+            enctype: 'multipart/form-data',
+            data: fd,
+            headers: {
+                'X-CSRFToken': csrf[0].value,
+            },
+            success: function (response) {
+                console.log('success', response);
+                // Additional success logic here
+            },
+            error: function (error) {
+                console.log('error', error);
+                // Error handling logic here
+            },
+            cache: false,
+            contentType: false,
+            processData: false,
+        });
+    })
+
 });
