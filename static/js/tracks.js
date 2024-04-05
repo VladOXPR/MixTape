792272816ad04f68bd4071d3e83c9e15bbfb6de5
vladvalchkov <vladvalchkov541@gmail.com>
2024-04-05 03:05:57 -0500

    Daw fix

--- static/js/tracks.js
+++ static/js/tracks.js
@@ -44,7 +44,7 @@ function createVis(trackId, mp3Url) {
         p.setup = function () {
             p.canvasWidth = p.song.duration(); // Makes the canvas width proportional to the length of the song
             p.createCanvas(p.canvasWidth * 3, 100); // Creates canvas
-            p.peaks = p.song.getPeaks(p.canvasWidth); // Gets the data pf the peaks to visually map out the song
+            p.peaks = p.song.getPeaks(p.canvasWidth * 100); // Gets the data pf the peaks to visually map out the song
             p.noFill();
         };
 
@@ -65,9 +65,11 @@ function createVis(trackId, mp3Url) {
             if (p.peaks) {
                 for (let i = 0; i < p.peaks.length; i++) {
                     let x = p.map(i, 0, p.peaks.length, 0, p.width);
-                    p.line(x, p.height / 2 + p.peaks[i] * 40, x, p.height / 2 - p.peaks[i] * 40);
+                    p.line(x, p.height / 2 + p.peaks[i] * 30, x, p.height / 2 - p.peaks[i] * 30);
                 }
             }
+
+            p.song.setVolume(isMuted ? 0 : 1);
 
             if (p.song.isPlaying()) {
                 posX = p.map(p.song.currentTime(), 0, p.song.duration(), 0, p.width);
@@ -195,19 +197,122 @@ function controlVis() {
                 togglePlayPauseAll(!isPlaying);
             }
         };
-
-        function togglePlayPauseAll(play) {
-            visInstances.forEach(vis => {
-                posM = vis.map(posX, 0, vis.width, 0, vis.song.duration());
-                if (play && !vis.song.isPlaying()) {
-                    vis.song.play();
-                    vis.song.jump(posM);
-                } else if (!play && vis.song.isPlaying()) {
-                    vis.song.pause();
-                }
-            });
-        }
     };
 
     new p5(control, 'control-container');
 }
+
+function togglePlayPauseAll(play) {
+    visInstances.forEach(vis => {
+        posM = vis.map(posX, 0, vis.width, 0, vis.song.duration());
+        if (play && !vis.song.isPlaying()) {
+            vis.song.play();
+            vis.song.jump(posM);
+        } else if (!play && vis.song.isPlaying()) {
+            vis.song.pause();
+        }
+    });
+}
+
+let micVis = function (p) {
+    let mic, recorder, soundFile;
+    let volhistory = [];
+    let recButton; // Single button for both actions
+    let isRecording = false; // Track if recording is active
+    let startTime = 0; // When the recording started
+    let recordedTime = 0; // Length of the recording
+    let initialPosX = 0; // Store initial posX when recording starts
+    let finalPosX = 0;
+
+    p.setup = function() {
+        let containerWidth = document.getElementById('codeModal').offsetWidth;
+        p.createCanvas(containerWidth, 100); // Set a fixed canvas size
+        mic = new p5.AudioIn();
+        mic.start();
+        recorder = new p5.SoundRecorder();
+        recorder.setInput(mic);
+        soundFile = new p5.SoundFile();
+        p.amp = new p5.Amplitude();
+
+        recButton = p.createButton('');
+        recButton.id('record-button');
+        recButton.parent('button-container');
+        recButton.mousePressed(p.toggleRecording);
+    };
+
+    p.draw = function() {
+        p.background(0);
+        let currentTime = 0;
+        let width = isRecording ? posX - initialPosX : finalPosX - initialPosX; // Example scaling, adjust as needed
+        p.fill(0); // Fill color for the "growing" rectangle
+        p.stroke(48, 54, 58);
+        p.strokeWeight(1);
+        p.rect(initialPosX, 1, width, p.height - 2, 10);
+
+        if (isRecording) {
+            if (volhistory.length < width) {
+                let vol = mic.getLevel();
+                volhistory.push(vol); // Add new volume data at intervals matching posX updates
+            }
+
+            p.stroke(255);
+
+            p.beginShape();
+            for (let i = 0; i < volhistory.length; i++) {
+                let x = p.map(i, 0, volhistory.length, initialPosX, initialPosX + width);
+                let y = p.map(volhistory[i] * 20, 0, 1, p.height / 2, 0);
+                p.vertex(x, y);
+            }
+            p.endShape();
+
+            p.beginShape();
+            for (let i = 0; i < volhistory.length; i++) {
+                let x = p.map(i, 0, volhistory.length, initialPosX, initialPosX + width);
+                let y = p.map(volhistory[i] * 20, 0, 1, p.height / 2, p.height); // Mirror mapping
+                p.vertex(x, y);
+            }
+            p.endShape();
+        }
+
+        p.stroke(255, 79, 0);
+        p.line(posX, 0, posX, p.height);
+    }
+
+    p.toggleRecording = function() {
+        if (!isRecording) {
+            p.record();
+            recButton.style('background-image', 'url("/media/recIng.png")');
+        } else {
+            p.stop();
+            recButton.style('background-image', 'url("/media/rec.png")');
+        }
+    };
+
+    p.record = function() {
+        if (!isRecording) {
+            p.userStartAudio();
+            recorder.record(soundFile);
+            isRecording = true;
+            startTime = window.performance.now();
+            recButton.html(''); // Update button text
+            initialPosX = posX;
+
+            togglePlayPauseAll(true);
+        }
+    };
+
+    p.stop = function() {
+        if (isRecording) {
+            recorder.stop();
+            isRecording = false;
+            recordedTime += (window.performance.now() - startTime) / 3000; // Update recordedTime
+            recButton.html(''); // Update button text back to 'Record'
+            volhistory = [];
+            finalPosX = posX;
+
+            togglePlayPauseAll(false);
+        }
+    };
+};
+
+let myMicVis = new p5(micVis, 'micVis');
